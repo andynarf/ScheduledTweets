063619b43ec94a866ad4c8cc203da7824c4884b2
Andres <c.andrespasso@gmail.com>
2022-04-29 03:54:57 -0300

    added sign up form, with validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  #GET /about
   get "about", to: "about#index"
 
-  #root route
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
 
   # Defines the root path route ("/")
