d22f27ac9ade2717ccb0c10849586fe038617b6b
Andres <c.andrespasso@gmail.com>
2022-05-09 14:20:42 -0300

    initial pahse finished

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,19 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   delete "logout", to: "sessions#destroy"
+
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 
   root to: "main#index"
 
