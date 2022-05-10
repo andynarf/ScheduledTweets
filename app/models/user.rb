2588c7aab9987c7fd9c2cd0485091762014853fc
Andres <c.andrespasso@gmail.com>
2022-05-10 16:06:02 -0300

    ya se asocia a la cuenta de twitter, omniauth funciona como deberia

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@
 #password_confirmation virtual
 
 class User < ApplicationRecord
+  has_many :twitter_accounts
+
   has_secure_password
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email address provided" }
+
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid email address provided" }
 end
